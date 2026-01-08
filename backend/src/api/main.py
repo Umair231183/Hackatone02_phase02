945caf6192c71d88c629f22e44b4ab36902e48d4
Umair231183 <hafizmuhammadumair016@gmail.com>
2026-01-08 19:47:51 +0500

    connection done

--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="Todo API", version="1.0.0")
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://hackatone02-phase02-hhzt.vercel.app/"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://hackatone02-phase02-hhzt.vercel.app/","https://hackatone02-phase02-hhzt-abxej5hgd-hafizmuhammadumairs-projects.vercel.app/"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allow_headers=["*"],
